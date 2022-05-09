845010f3b3705a024b9268b61740c5f63802c25d
Deiring Andreas <andreas@deiring.at>
2022-05-09 15:25:30 +0200

    indent mismatch

--- src/twincat/ads/AdsNotification.java
+++ src/twincat/ads/AdsNotification.java
@@ -7,53 +7,53 @@ public class AdsNotification {
     /*** global constant variable ****/
     /*********************************/
 
-	public static final int TIME_RATIO_NS_TO_MS = 10000;
+    public static final int TIME_RATIO_NS_TO_MS = 10000;
 
     /*********************************/
     /******** global variable ********/
     /*********************************/
 
-	private long dataLength = 0;
+    private long dataLength = 0;
 
-	private long maxDelay = 0;
+    private long maxDelay = 0;
 	
-	private long cycleTime = 0;
+    private long cycleTime = 0;
 	
-	private TransmitMode transmissionMode = TransmitMode.UNKNOWN;
+    private TransmitMode transmissionMode = TransmitMode.UNKNOWN;
 
     /*********************************/
     /******** setter & getter ********/
     /*********************************/
 
-	public long getDataLength() {
-		return dataLength;
-	}
+    public long getDataLength() {
+        return dataLength;
+    }
 
-	public void setDataLength(long dataLength) {
-		this.dataLength = dataLength;
-	}
+    public void setDataLength(long dataLength) {
+        this.dataLength = dataLength;
+    }
 	
-	public long getMaxDelay() {
-		return maxDelay;
-	}
+    public long getMaxDelay() {
+        return maxDelay;
+    }
 
-	public void setMaxDelay(long maxDelay) {
-		this.maxDelay = maxDelay;
-	}
+    public void setMaxDelay(long maxDelay) {
+        this.maxDelay = maxDelay;
+    }
 
-	public long getCycleTime() {
-		return cycleTime;
-	}
+    public long getCycleTime() {
+        return cycleTime;
+    }
 
-	public void setCycleTime(long cycleTime) {
-		this.cycleTime = cycleTime;
-	}
+    public void setCycleTime(long cycleTime) {
+        this.cycleTime = cycleTime;
+    }
 
-	public TransmitMode getTransmissionMode() {
-		return transmissionMode;
-	}
+    public TransmitMode getTransmissionMode() {
+        return transmissionMode;
+    }
 
-	public void setTransmissionMode(TransmitMode transmissionMode) {
-		this.transmissionMode = transmissionMode;
-	}
+    public void setTransmissionMode(TransmitMode transmissionMode) {
+        this.transmissionMode = transmissionMode;
+    }
 }
