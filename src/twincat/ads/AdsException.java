879a314ec66c3923204b9ac31b0232f271440957
Deiring Andreas <andreas@deiring.at>
2022-05-09 15:24:37 +0200

    indent mismatch

--- src/twincat/ads/AdsException.java
+++ src/twincat/ads/AdsException.java
@@ -7,43 +7,43 @@ public class AdsException extends Exception {
     /**** local constant variable ****/
     /*********************************/
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     /*********************************/
     /******** global variable ********/
     /*********************************/
 
-	private AdsError adsError;
+    private AdsError adsError;
 
     /*********************************/
     /********** constructor **********/
     /*********************************/
 
-	public AdsException(AdsError adsError) {
-		this.adsError = adsError;
-	}
+    public AdsException(AdsError adsError) {
+        this.adsError = adsError;
+    }
 
-	public AdsException(long adsError) {
-		this.adsError = AdsError.getByValue((int) adsError);
-	}
+    public AdsException(long adsError) {
+        this.adsError = AdsError.getByValue((int) adsError);
+    }
 
     /*********************************/
     /******** setter & getter ********/
     /*********************************/
 
-	public AdsError getAdsError() {
-		return adsError;
-	}
+    public AdsError getAdsError() {
+        return adsError;
+    }
 
-	public void setAdsError(AdsError adsError) {
-		this.adsError = adsError;
-	}
+    public void setAdsError(AdsError adsError) {
+        this.adsError = adsError;
+    }
 
     /*********************************/
     /********* public method *********/
     /*********************************/
 
-	public String getAdsErrorMessage() {
-		return adsError.toString();
-	}
+    public String getAdsErrorMessage() {
+        return adsError.toString();
+    }
 }
